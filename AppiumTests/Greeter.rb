5b3e5dde6a1b1f43d21a7f05daaed030c01a9eaa
Henrik Hodne <me@henrikhodne.com>
2013-08-06 17:07:04 -0700

    Use Sauce Labs for Appium

--- AppiumTests/Greeter.rb
+++ AppiumTests/Greeter.rb
@@ -1,14 +1,21 @@
 require "rspec"
 require "selenium-webdriver"
+require "json"
+require "rest_client"
 
-server_url = "http://127.0.0.1:4723/wd/hub"
+APP_PATH = "sauce-storage:Greeter.app.zip"
+SAUCE_USERNAME = ENV["SAUCE_USERNAME"]
+SAUCE_ACCESS_KEY = ENV["SAUCE_ACCESS_KEY"]
+AUTH_DETAILS = "#{SAUCE_USERNAME}:#{SAUCE_ACCESS_KEY}"
 
 describe "Greeter" do
-  before :all do
-    @driver = Selenium::WebDriver.for(:remote, desired_capabilities: { 'browserName' => 'iOS', 'platform' => 'Mac', 'version' => '6.1' }, url: server_url)
+  before :each do
+    @driver = Selenium::WebDriver.for(:remote, desired_capabilities: capabilities, url: server_url)
   end
 
-  after :all do
+  after :each do
+    job_id = @driver.send(:bridge).session.id
+    update_job_success(job_id, example.exception.nil?)
     @driver.quit
   end
 
@@ -16,4 +23,28 @@ describe "Greeter" do
     greeting = @driver.find_elements(:tag_name, 'staticText')[0].text
     expect(greeting).to eq("Hello, world!")
   end
+end
+
+def capabilities
+  {
+    "browserName" => "iOS 6.1",
+    "platform" => "Mac 10.8",
+    "device" => "iPhone Simulator",
+    "app" => APP_PATH,
+    "name" => "Greeter tests for Appium",
+  }
+end
+
+def server_url
+  "http://#{AUTH_DETAILS}@ondemand.saucelabs.com:80/wd/hub"
+end
+
+def rest_jobs_url
+  "https://#{AUTH_DETAILS}@saucelabs.com/rest/v1/#{SAUCE_USERNAME}/jobs"
+end
+
+# Because WebDriver doesn't have the concept of test failure, use the Sauce
+# Labs REST API to record job success or failure
+def update_job_success(job_id, success)
+    RestClient.put "#{rest_jobs_url}/#{job_id}", {"passed" => success}.to_json, :content_type => :json
 end
